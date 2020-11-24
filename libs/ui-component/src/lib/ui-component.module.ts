f89f2a6ceb92912c5edc2ecb2af510d36608ee17
Nathan Samsoedien <contact@samsoedien.com>
2020-11-24 16:41:46 +0100

    first commit

---
+++ libs/ui-component/src/lib/ui-component.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { UiComponentComponent } from './ui-component/ui-component.component';
+
+@NgModule({
+  imports: [CommonModule],
+  declarations: [UiComponentComponent],
+})
+export class UiComponentModule {}
---
+++ libs/ui-component/src/lib/ui-component/ui-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'nx-angular-app-ui-component',
+  templateUrl: './ui-component.component.html',
+  styleUrls: ['./ui-component.component.scss']
+})
+export class UiComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
